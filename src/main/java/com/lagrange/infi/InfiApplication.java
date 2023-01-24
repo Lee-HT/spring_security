45a53964cababbdc77ea2791f0614d1288a1caae
Lee-HT <12gusxkr@naver.com>
2023-01-24 19:40:19 +0900

    default

---
+++ src/main/java/com/lagrange/infi/InfiApplication.java
@@ -0,0 +1,13 @@
+package com.lagrange.infi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiApplication.class, args);
+	}
+
+}
