b6c6660b14160291c7b57b4c0809cb62d1fbdeda
LEE-HT <12gusxkr@naver.com>
2023-01-30 18:03:18 +0900

    register continue

--- src/main/java/com/lagrange/infi/data/service/impl/MemberServiceImpl.java
+++ src/main/java/com/lagrange/infi/data/service/impl/MemberServiceImpl.java
@@ -4,11 +4,13 @@ import com.lagrange.infi.data.dto.MemberD;
 import com.lagrange.infi.data.entity.MemberE;
 import com.lagrange.infi.data.repository.MemberRepository;
 import com.lagrange.infi.data.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 //@RequiredArgsConstructor //final , notnull 어노테이션 붙은 객체에 생성자 자동 주입
 public class MemberServiceImpl implements MemberService {
     private final PasswordEncoder passwordEncoder;
@@ -23,6 +25,7 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public MemberD register(String id,String password){
         String encodedPassword = passwordEncoder.encode(password);
+        log.info(id,password);
         MemberE memberE = MemberE.builder()
 //                .idx(1L)
                 .id(id)
