4ece3d999a11b5004ae7264b4b51d163dad375c0
LEE-HT <12gusxkr@naver.com>
2023-01-30 17:33:06 +0900

    datetime createat

--- src/main/java/com/lagrange/infi/controller/LoginController.java
+++ src/main/java/com/lagrange/infi/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.lagrange.infi.data.entity.MemberE;
 import com.lagrange.infi.data.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,38 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/logins/")
 public class LoginController {
 
-    MemberService memberService;
+    private MemberService memberService;
 
+    @Autowired
     public LoginController(MemberService memberService){
         this.memberService = memberService;
     }
 
     @PostMapping("signup")
-    public ResponseEntity<MemberD> register(@Valid @RequestBody MemberD memberD){
+    public ResponseEntity<MemberD> register(@RequestBody String id, @RequestBody String password){
         try{
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(memberService.register(memberD.getId(),memberD.getPassword()));
+                    .body(memberService.register(id,password));
         } catch (Exception e) {
             log.info(e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(memberD);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MemberD(0L,id,password));
         }
     }
 
     @PostMapping("update")
-    public ResponseEntity<MemberD> update(@Valid @RequestBody MemberD memberS){
+    public ResponseEntity<MemberD> update(@Valid @RequestBody MemberD memberD){
         try{
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(memberService.update(memberS.getIdx(),memberS.getId(),memberS.getPassword()));
+                    .body(memberService.update(memberD.getIdx(),memberD.getId(),memberD.getPassword()));
         }catch (Exception e) {
             log.info(e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(memberS);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(memberD);
         }
     }
 
     @PostMapping("signin")
     public ResponseEntity login(@RequestBody String id,String password){
         try{
-            return new ResponseEntity(memberService.login(id,password),HttpStatus.OK);
+            return ResponseEntity.status(HttpStatus.OK).body(memberService.login(id,password));
         } catch (Exception e) {
             log.error(e.getMessage());
             return new ResponseEntity("fail",
