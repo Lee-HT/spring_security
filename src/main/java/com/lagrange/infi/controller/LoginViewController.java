2a9c6243b1600f83834573bc4bc98458d4a855d4
Lee-HT <12gusxkr@naver.com>
2023-02-07 19:03:00 +0900

    secured annotation

--- src/main/java/com/lagrange/infi/controller/LoginViewController.java
+++ src/main/java/com/lagrange/infi/controller/LoginViewController.java
@@ -2,7 +2,10 @@ package com.lagrange.infi.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 @RequestMapping(value = "/df/")
-@Tag(name = "Hello Controller",description = "테스트용")
-public class HelloController
+@Tag(name = "LoginView Controller",description = "테스트용")
+public class LoginViewController
 {
     @GetMapping("test")
+    @Secured({"ROLE_MANAGER","ROLE_ADMIN"})
     @Operation(summary = "hello.mustache",description = "view")
     public String hello(){
         return "hello";
