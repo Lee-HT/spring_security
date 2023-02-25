df47f36e419cd963d699decf24d3ed35af131ee5
Lee-HT <12gusxkr@naver.com>
2023-02-25 18:15:45 +0900

    application 설명 추가

--- src/main/java/com/lagrange/infi/InfiApplication.java
+++ src/main/java/com/lagrange/infi/InfiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+//SecurityAutoConfiguration.class를 제외하면 security 자동 설정 off 됨
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class InfiApplication {
 
